d702a036cda3ba0073320195f0fa8ee4b057550e
Robin Bron <admin@finwo.nl>
2016-04-22 21:52:36 +0200

    Project init

---
+++ src/Event.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Finwo\Hooked;
+
+use Finwo\PropertyAccessor\PropertyAccessor;
+
+class Event
+{
+    /**
+     * @var string
+     */
+    protected $name = '';
+
+    /**
+     * @var array
+     */
+    protected $param = array();
+
+    /**
+     * @var PropertyAccessor
+     */
+    private $accessor;
+
+    /**
+     * @return PropertyAccessor
+     */
+    private function getAccessor()
+    {
+        if (is_null($this->accessor)) {
+            $this->accessor = new PropertyAccessor(true);
+        }
+        return $this->accessor;
+    }
+
+    /**
+     * @param $key
+     * @return array|mixed|null
+     * @throws \Exception
+     */
+    public function get($key)
+    {
+        // Try directly, might be the quickest way
+        if (isset($this->{$key})) {
+            return $this->{$key};
+        }
+
+        // Look deeper
+        $accessor = $this->getAccessor();
+        return $accessor->get($this, $key, '.');
+    }
+
+    public function set($key, $value)
+    {
+        // Try directly, might be the quickest way
+        if (isset($this->{$key})) {
+            $this->{$key} = $value;
+            return $this;
+        }
+
+        // Look deeper
+        $accessor = $this->getAccessor();
+        $accessor->set($this, $key, $value, '.');
+        return $this;
+    }
+}
---
+++ src/Hooked.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Finwo\Hooked;
+
+use Invoker\Invoker;
+
+abstract class Hooked
+{
+    /**
+     * @var array
+     */
+    protected $hooks = array();
+
+    /**
+     * @var Invoker
+     */
+    protected $invoker;
+
+    protected function getInvoker()
+    {
+        if (is_null($this->invoker)) {
+            $this->invoker = new Invoker();
+        }
+        return $this->invoker;
+    }
+
+    /**
+     * Add a hook the the element
+     *
+     * @param string $key
+     * @param \Closure $callback
+     * @return $this|bool
+     */
+    public function addHook( $key = '', \Closure $callback = null )
+    {
+        // Make sure the key is valid
+        if (is_string($key)) {
+            return false;
+        }
+
+        // Make sure the callback is valid
+        if (!is_callable($callback)) {
+            return false;
+        }
+
+        // Make sure the hook entry exists
+        if (isset($this->hooks[$key])) {
+            $this->hooks[$key] = array();
+        }
+
+        // Store hook
+        $this->hooks[$key][] = $callback;
+
+        // Return ourselves
+        return $this;
+    }
+
+    /**
+     * Runs all hooks on a certain key
+     *
+     * @param  Event  $event
+     *
+     * @return Hooked $this
+     */
+    protected function dispatch( Event $event )
+    {
+        // Pre-define some required stuff
+        $invoker = $this->getInvoker();
+        $name    = $event->get('name');
+
+        // Make sure the hook entry exists
+        if (!isset($this->hooks[$name])) {
+            $this->hooks[$name] = array();
+        }
+
+        foreach ($this->hooks[$name] as $hook) {
+
+            // Call the hook
+            if (!$invoker->call($hook, array(
+                'event' => $event
+            ))) {
+                // Break propegation if asked to
+                break;
+            }
+        }
+
+        return $this;
+    }
+}
